8da21edb84e617be2f3e7cbce70a19e7eff9e1d2
Gescobar1603 <70909459+Gescobar1603@users.noreply.github.com>
2021-04-28 02:23:13 -0500

    gg

---
+++ src/app/entities/top-banner.ts
@@ -0,0 +1,9 @@
+export class TopBanner {
+
+  H3_tag: string
+  P1_tag: string
+  H4_tag: string
+  P2_tag: string
+  Span
+
+}
---
+++ src/app/tests/top-banner.spec.ts
@@ -0,0 +1,7 @@
+import { TopBanner } from '../entities/top-banner';
+
+describe('TopBanner', () => {
+  it('should create an instance', () => {
+    expect(new TopBanner()).toBeTruthy();
+  });
+});
