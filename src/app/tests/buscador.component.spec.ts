3d9b3229b43eaf9c4ae9fd76fc7cb0cf0063c219
Gescobar1603 <70909459+Gescobar1603@users.noreply.github.com>
2021-06-10 19:42:29 -0500

    GG

--- src/app/tests/buscador.component.spec.ts
+++ src/app/tests/buscador.component.spec.ts
@@ -1,6 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { BuscadorComponent } from './buscador.component';
+import { BuscadorComponent } from '../components/buscador.component';
 
 describe('BuscadorComponent', () => {
   let component: BuscadorComponent;
